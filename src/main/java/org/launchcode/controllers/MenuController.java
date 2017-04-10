cc3ba5f540ce6237c392fe4be61b21102cc600d6
jayhenderson <jjhenderson@gmail.com>
2017-04-10 18:21:28 -0500

    Add controller and html for new menus

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,11 +1,19 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+import static java.awt.SystemColor.menu;
 
 /**
  * Created by J on 4/10/2017.
@@ -25,7 +33,28 @@ public class MenuController {
 
         model.addAttribute("menus", menuDao.findAll());
 
-        return null;
+        return "menu/index.html";
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model){
+
+        model.addAttribute(new Menu());
+
+        return "menu/add.html";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid Menu menu, Errors errors){
+        if (errors.hasErrors()){
+            model.addAttribute(menu);
+            model.addAttribute(errors);
+            return "menu/add.html";
+        }
+        menuDao.save(menu);
+        return "redirect:view/" + menu.getId();
+    }
+
+
 
 }
